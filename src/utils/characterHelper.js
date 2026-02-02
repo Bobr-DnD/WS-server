7940a9ec46845638a8bd969f52b93235d95dd687
CyberpOOP <vlad0021135@gmail.com>
2026-02-02 17:13:52 +0200

    fix concatenation for characteristics

--- src/utils/characterHelper.js
+++ src/utils/characterHelper.js
@@ -1,6 +1,18 @@
 export function applyEffects(character) {
-    character._characteristicsComputed = structuredClone(character.characteristics)
+    character._characteristicsComputed = Object.fromEntries(
+        Object.entries(character.characteristics).map(([key, value]) => {
+            if (!Number.isNaN(Number(value))) {
+                return [key, Number(value)]
+            }
+            return [key, value]
+        })
+    )
+
     character.effects.forEach(effect => {
-        Object.entries(effect.effect).map(([key, value]) => character._characteristicsComputed[key] += value)
+        Object.entries(effect.effect).map(([key, value]) => {
+            if(!Number.isNaN(Number(character._characteristicsComputed[key]))) character._characteristicsComputed[key] += value
+        
+        //TODO copy from API server
+        })
     });
 }
